c0211eade77eaf448f4710c67aca6a0d64b9a997
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-12-15 19:18:57 +0100

    Create example

---
+++ pages/api/v1/message.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const handler = (req, res) => {
+  res.status(200).json({ msgSuccess: 'Hello, you fetch data!' });
+};
+
+export default handler;
